111619f467ff87c6fb07ba20b35be70189745b33
giorgip62 <giorgiop62@gmail.com>
2022-10-12 18:11:56 +0200

    prova

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*const btnInput = document.getElementById('btn-input1');
+btnInput.addEventListener('click', function(){
+  const name = prompt('Nome e Cgonome')
+  console.log(name);
+  console.log(document.getElementById('input').value);
+  document.getElementById('input').value = name;
+});
+
+const btnInput1 = document.getElementById('btn-input');
+btnInput2.addEventListener('click', function(){
+  const name = document.getElementById('input').value;
+  //alert('Nell\'input c\'è scritto: ' + name);
+  document.getElementById('output-name').innerHTML = name;
+
+  //resetto i lvalue dell'input
+  document.getElementById('input').value = '';
+});
